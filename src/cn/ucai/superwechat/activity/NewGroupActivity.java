dfbc0a66b957d577c3ca6e5999a127342819f7b1
zhyangzzz <522137140@qq.com>
2016-07-26 15:15:04 +0800

    新建群组获取hxid

--- src/cn/ucai/superwechat/activity/NewGroupActivity.java
+++ src/cn/ucai/superwechat/activity/NewGroupActivity.java
@@ -17,25 +17,38 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.easemob.chat.EMGroup;
 import com.easemob.chat.EMGroupManager;
+
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.listener.OnSetAvatarListener;
+
 import com.easemob.exceptions.EaseMobException;
 
 public class NewGroupActivity extends BaseActivity {
+	private final static String TAG = NewGroupActivity.class.getSimpleName();
 	private EditText groupNameEditText;
 	private ProgressDialog progressDialog;
 	private EditText introductionEditText;
 	private CheckBox checkBox;
 	private CheckBox memberCheckbox;
 	private LinearLayout openInviteContainer;
+	private ImageView avater;
+	private OnSetAvatarListener mOnSetAvatarListener;
+	private String avatarName;
+
+	private static int CREATE_GROUP = 100;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +59,7 @@ public class NewGroupActivity extends BaseActivity {
 		checkBox = (CheckBox) findViewById(R.id.cb_public);
 		memberCheckbox = (CheckBox) findViewById(R.id.cb_member_inviter);
 		openInviteContainer = (LinearLayout) findViewById(R.id.ll_open_invite);
-		
+		avater = (ImageView) findViewById(R.id.iv_avatar);
 		checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			
 			@Override
@@ -58,6 +71,17 @@ public class NewGroupActivity extends BaseActivity {
 				}
 			}
 		});
+		findViewById(R.id.layout_group_icon).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				mOnSetAvatarListener = new OnSetAvatarListener(NewGroupActivity.this,R.id.layout_group,getAvatarName(), I.AVATAR_TYPE_GROUP_PATH);
+			}
+		});
+	}
+
+	private String getAvatarName() {
+		avatarName = String.valueOf(System.currentTimeMillis());
+		return avatarName;
 	}
 
 	/**
@@ -72,7 +96,7 @@ public class NewGroupActivity extends BaseActivity {
 			startActivity(intent);
 		} else {
 			// 进通讯录选人
-			startActivityForResult(new Intent(this, GroupPickContactsActivity.class).putExtra("groupName", name), 0);
+			startActivityForResult(new Intent(this, GroupPickContactsActivity.class).putExtra("groupName", name), CREATE_GROUP);
 		}
 	}
 	
@@ -81,7 +105,15 @@ public class NewGroupActivity extends BaseActivity {
 		super.onActivityResult(requestCode, resultCode, data);
 		String st1 = getResources().getString(R.string.Is_to_create_a_group_chat);
 		final String st2 = getResources().getString(R.string.Failed_to_create_groups);
-		if (resultCode == RESULT_OK) {
+		Log.e(TAG,"requestCode="+requestCode);
+		if (resultCode!=RESULT_OK){
+			return;
+		}
+		mOnSetAvatarListener.setAvatar(requestCode,data,avater);
+		if (requestCode==OnSetAvatarListener.REQUEST_CROP_PHOTO){
+			//createAppGroup();
+		}
+		if (requestCode == CREATE_GROUP) {
 			//新建群组
 			progressDialog = new ProgressDialog(this);
 			progressDialog.setMessage(st1);
@@ -95,15 +127,17 @@ public class NewGroupActivity extends BaseActivity {
 					String groupName = groupNameEditText.getText().toString().trim();
 					String desc = introductionEditText.getText().toString();
 					String[] members = data.getStringArrayExtra("newmembers");
+					EMGroup group;
 					try {
 						if(checkBox.isChecked()){
 							//创建公开群，此种方式创建的群，可以自由加入
 							//创建公开群，此种方式创建的群，用户需要申请，等群主同意后才能加入此群
-						    EMGroupManager.getInstance().createPublicGroup(groupName, desc, members, true,200);
+							group = EMGroupManager.getInstance().createPublicGroup(groupName, desc, members, true,200);
 						}else{
 							//创建不公开群
-						    EMGroupManager.getInstance().createPrivateGroup(groupName, desc, members, memberCheckbox.isChecked(),200);
+							group = EMGroupManager.getInstance().createPrivateGroup(groupName, desc, members, memberCheckbox.isChecked(),200);
 						}
+						Log.e(TAG,"hxid="+group.getGroupId());
 						runOnUiThread(new Runnable() {
 							public void run() {
 								progressDialog.dismiss();
@@ -115,7 +149,7 @@ public class NewGroupActivity extends BaseActivity {
 						runOnUiThread(new Runnable() {
 							public void run() {
 								progressDialog.dismiss();
-								Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), 1).show();
+								Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
 							}
 						});
 					}
