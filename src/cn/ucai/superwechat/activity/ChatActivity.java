bb0262690ae629ac81215c3c24050ce87b7ab338
zhyangzzz <522137140@qq.com>
2016-07-27 14:13:24 +0800

    调用task类使进入群聊界面时下载群组成员

--- src/cn/ucai/superwechat/activity/ChatActivity.java
+++ src/cn/ucai/superwechat/activity/ChatActivity.java
@@ -22,8 +22,10 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
@@ -96,6 +98,7 @@ import cn.ucai.superwechat.adapter.ExpressionPagerAdapter;
 import cn.ucai.superwechat.adapter.MessageAdapter;
 import cn.ucai.superwechat.adapter.VoicePlayClickListener;
 import cn.ucai.superwechat.domain.RobotUser;
+import cn.ucai.superwechat.task.DownloadMemberMapTask;
 import cn.ucai.superwechat.utils.CommonUtils;
 import cn.ucai.superwechat.utils.ImageUtils;
 import cn.ucai.superwechat.utils.SmileUtils;
@@ -424,6 +427,7 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 	            forwardMessage(forward_msg_id);
 	        }
 		}
+		setUpdateMemberListener();
 	}
 
 	protected void onConversationInit(){
@@ -519,6 +523,7 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
         }else{
             ((TextView) findViewById(R.id.name)).setText(toChatUsername);
         }
+		new DownloadMemberMapTask(getApplicationContext(),toChatUsername).execute();
         
         // 监听当前会话的群聊解散被T事件
         groupListener = new GroupListener();
@@ -1472,6 +1477,9 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 		if(groupListener != null){
 		    EMGroupManager.getInstance().removeGroupChangeListener(groupListener);
 		}
+		if(mReceiver!=null){
+			unregisterReceiver(mReceiver);
+		}
 	}
 
 	@Override
@@ -1720,7 +1728,7 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 
 				public void run() {
 					if (toChatUsername.equals(groupId)) {
-						Toast.makeText(ChatActivity.this, st13, 1).show();
+						Toast.makeText(ChatActivity.this, st13, Toast.LENGTH_SHORT).show();
 						if (GroupDetailsActivity.instance != null)
 							GroupDetailsActivity.instance.finish();
 						finish();
@@ -1737,7 +1745,7 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 
 				public void run() {
 					if (toChatUsername.equals(groupId)) {
-						Toast.makeText(ChatActivity.this, st14, 1).show();
+						Toast.makeText(ChatActivity.this, st14, Toast.LENGTH_SHORT).show();
 						if (GroupDetailsActivity.instance != null)
 							GroupDetailsActivity.instance.finish();
 						finish();
@@ -1748,12 +1756,25 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 
 	}
 
+
 	public String getToChatUsername() {
 		return toChatUsername;
 	}
 
 	public ListView getListView() {
 		return listView;
+	}
+	class UpdateMemberListener extends BroadcastReceiver{
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			adapter.notifyDataSetChanged();
+		}
+	}
+	UpdateMemberListener mReceiver;
+	private void setUpdateMemberListener(){
+		mReceiver = new UpdateMemberListener();
+		IntentFilter filter = new IntentFilter("update_member_list");
+		registerReceiver(mReceiver,filter);
 	}
 
 }
