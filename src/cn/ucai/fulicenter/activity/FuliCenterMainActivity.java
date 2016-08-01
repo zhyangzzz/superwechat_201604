8d9096ab5a4e40b6deb0a0a18f94eb7853deeb01
zhyangzzz <522137140@qq.com>
2016-08-01 16:15:52 +0800

    底部菜单的切换事件

--- src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
+++ src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
@@ -1,7 +1,11 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.BaseAdapter;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import cn.ucai.fulicenter.R;
 
@@ -9,10 +13,69 @@ import cn.ucai.fulicenter.R;
  * Created by Administrator on 2016/8/1.
  */
 public class FuliCenterMainActivity extends BaseActivity{
+    private static final String TAG = FuliCenterMainActivity.class.getSimpleName();
+    RadioButton rbNewGood,rbBoutique,rbCategory,rbCart,rbPersonalCenter;
+    TextView tvCartHint;
+    RadioButton[] mrbTabs;
+    int index;
+    int currentIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fulicenter_main);
+        initView();
 
+    }
+
+    private void initView() {
+        rbNewGood = (RadioButton) findViewById(R.id.layout_new_good);
+        rbBoutique = (RadioButton) findViewById(R.id.layout_boutique);
+        rbCategory = (RadioButton) findViewById(R.id.layout_category);
+        rbCart = (RadioButton) findViewById(R.id.layout_cart);
+        rbPersonalCenter = (RadioButton) findViewById(R.id.layout_personal_center);
+
+        tvCartHint = (TextView) findViewById(R.id.tvCartHint);
+        mrbTabs = new RadioButton[5];
+        mrbTabs[0] = rbNewGood;
+        mrbTabs[1] = rbBoutique;
+        mrbTabs[2] = rbCategory;
+        mrbTabs[3] = rbCart;
+        mrbTabs[4] = rbPersonalCenter;
+    }
+
+    public void onCheckedChange(View view){
+        switch (view.getId()){
+            case R.id.layout_new_good:
+                index = 0;
+                break;
+            case R.id.layout_boutique:
+                index = 1;
+                break;
+            case R.id.layout_category:
+                index = 2;
+                break;
+            case R.id.layout_cart:
+                index = 3;
+                break;
+            case R.id.layout_personal_center:
+                index = 4;
+                break;
+        }
+        Log.e(TAG,"index="+index+",currentIndex="+currentIndex);
+        if (index!=currentIndex){
+            setRadioButtonStatus(index);
+            currentIndex = index;
+        }
+    }
+
+    private void setRadioButtonStatus(int index) {
+        for (int i=0;i<mrbTabs.length;i++){
+            if (index==i){
+                mrbTabs[i].setChecked(true);
+            }else{
+                mrbTabs[i].setChecked(false);
+            }
+        }
     }
 }
