f401c3bb34d103cdb6ad9c02905d65f4288b9a6c
zhyangzzz <522137140@qq.com>
2016-08-02 10:00:03 +0800

    新品Fragment的初始化布局

--- src/cn/ucai/fulicenter/activity/NewGoodFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodFragment.java
@@ -2,29 +2,60 @@ package cn.ucai.fulicenter.activity;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.adapter.GoodAdapter;
+import cn.ucai.fulicenter.bean.NewGoodBean;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class NewGoodFragment extends Fragment {
-
-
+    FuliCenterMainActivity mContext;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerView;
+    GridLayoutManager mGridLayoutManager;
+    GoodAdapter mAdapter;
+    List<NewGoodBean> mGoodList;
     public NewGoodFragment() {
-        // Required empty public constructor
+
     }
 
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_good, container, false);
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mContext = (FuliCenterMainActivity) getContext();
+        View layout = View.inflate(mContext, R.layout.fragment_new_good, null);
+        mGoodList = new ArrayList<NewGoodBean>();
+        initView(layout);
+        return layout;
+    }
+
+    private void initView(View layout) {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(R.id.srl_new_good);
+        mSwipeRefreshLayout.setColorSchemeColors(
+                R.color.google_blue,
+                R.color.google_yellow,
+                R.color.google_red,
+                R.color.google_green
+        );
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.rv_new_good);
+        mGridLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);
+        mGridLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mAdapter = new GoodAdapter(mContext,mGoodList);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 }
