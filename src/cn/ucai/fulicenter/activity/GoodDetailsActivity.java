c5e22751a9a201fed6d1a8486ada026f50c08f74
zhyangzzz <522137140@qq.com>
2016-08-03 15:29:26 +0800

    获取商品详情数据并显示

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -6,9 +6,13 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import cn.ucai.fulicenter.D;
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.GoodDetailsBean;
+import cn.ucai.fulicenter.data.OkHttpUtils2;
 import cn.ucai.fulicenter.view.FlowIndicator;
 import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
@@ -23,12 +27,14 @@ public class GoodDetailsActivity extends BaseActivity{
     FlowIndicator mFlowIndicator;
     WebView wvGoodBrief;
     int mGoodId;
+    GoodDetailsActivity mContext;
 
 
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setContentView(R.layout.activity_good_details);
+        mContext = GoodDetailsActivity.this;
         initView();
         initData();
     }
@@ -36,6 +42,42 @@ public class GoodDetailsActivity extends BaseActivity{
     private void initData() {
         mGoodId = getIntent().getIntExtra(D.GoodDetails.KEY_GOODS_ID,0);
         Log.e(TAG,"mGoodId="+mGoodId);
+        if(mGoodId>0){
+            getGoodDetailsByGroupId(new OkHttpUtils2.OnCompleteListener<GoodDetailsBean>() {
+                @Override
+                public void onSuccess(GoodDetailsBean result) {
+                    Log.e(TAG,"result="+result);
+                    if(result!=null){
+                        showGoodDetails(result);
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+                    Log.e(TAG,"error="+error);
+                    finish();
+                    Toast.makeText(mContext, "获取商品详情数据失败!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            finish();
+            Toast.makeText(mContext, "获取商品详情数据失败!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showGoodDetails(GoodDetailsBean details) {
+        tvGoodEnglishName.setText(details.getGoodsEnglishName());
+        tvGoodName.setText(details.getGoodsName());
+        tvGoodPriceShop.setText(details.getShopPrice());
+        tvGoodPriceCurrent.setText(details.getCurrencyPrice());
+    }
+
+    private void getGoodDetailsByGroupId(OkHttpUtils2.OnCompleteListener<GoodDetailsBean> listener){
+        OkHttpUtils2<GoodDetailsBean> utils = new OkHttpUtils2<GoodDetailsBean>();
+        utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
+                .addParam(D.GoodDetails.KEY_GOODS_ID,String.valueOf(mGoodId))
+                .targetClass(GoodDetailsBean.class)
+                .execute(listener);
     }
 
     private void initView() {
